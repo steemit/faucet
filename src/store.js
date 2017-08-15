99a5f9e7c1b3c1681989ef808cff5db82faba329
adcpm <fabien@bonustrack.co>
2017-08-15 14:39:16 +0700

    Add empty reducer

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
+const reducers = combineReducers({});
+
 const store = createStore(
+  reducers,
   window.devToolsExtension && window.devToolsExtension(),
   applyMiddleware(thunk),
 );
