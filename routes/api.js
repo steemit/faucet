82788a088a5e4580d06a2df73348ce5718fa03ba
adcpm <fabien@bonustrack.co>
2017-08-15 22:37:06 +0700

    Create route for phone confirmation

--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,18 @@ router.get('/', (req, res) => {
   res.json({});
 });
 
+router.get('/submit_phone', (req, res) => {
+  const { countryCode, phoneNumber } = req.query;
+  req.twilio.messages.create({
+    body: '12345 is your SteemConnect confirmation code',
+    to: `+${countryCode}${phoneNumber}`,
+    from: '+12062028357',
+  }).then((result) => {
+    res.json(result);
+  }).catch((error) => {
+    const status = error.status || 400;
+    res.status(status).json(error);
+  });
+});
+
 module.exports = router;
