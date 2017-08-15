fd2b4daaa0ca5d76ae32e02af6bcba529e2d92e0
adcpm <fabien@bonustrack.co>
2017-08-15 23:08:42 +0700

    Separate email templates in json file

--- helpers/mail.js
+++ helpers/mail.js
@@ -1,23 +1,10 @@
+const templates = require('./templates.json');
 const mailgun = require('mailgun-js')({
   apiKey: process.env.MAILGUN_API_KEY,
   domain: process.env.MAILGUN_DOMAIN,
 });
 
 const mail = {};
-
-const templates = {
-  confirm_email: {
-    from: 'SteemConnect <noreply@steemconnect.com>',
-    subject: 'Welcome to SteemConnect!',
-    text:
-    'Thanks for signing up to SteemConnect, we are excited to have you on board!\n' +
-    'To complete and confirm your registration to SteemConnect, you need to verify your email address.\n\n' +
-    '{url} \n\n' +
-    'If you encounter any problem using the service, contact us and a member of our team will help you.\n\n' +
-    'Cheers,\n' +
-    'SteemConnect',
-  },
-};
 
 mail.send = (to, template, params = {}, cb) => {
   const data = templates[template];
