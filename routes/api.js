42def4da1c5ca876c3252c7f76ade1cfaf7de23d
Grégory LATINIER <cehraphaim@live.fr>
2017-08-16 14:29:49 +0200

    Add email existance verification and insert user in db

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,43 @@ router.get('/', (req, res) => {
   res.json({});
 });
 
-router.get('/request_email', (req, res) => {
-  res.json({});
+router.get('/request_email', async (req, res) => {
+  const userCount = await req.db.users.count({
+    where: {
+      email: req.query.email,
+      email_is_verified: true,
+    },
+  });
+
+  if (userCount > 0) {
+    res.status(400).json({ error: 'Email already used.' });
+  } else {
+    const userExist = await req.db.users.count({
+      where: {
+        email: req.query.email,
+      },
+    });
+
+    if (userExist === 0) {
+      req.db.users.create({
+        email: req.query.email,
+        email_is_verified: false,
+        last_attempt_verify_email: null,
+        phone_number: '',
+        phone_number_is_verified: false,
+        last_attempt_verify_phone_number: null,
+        ip: req.connection.remoteAddress,
+        ua: req.headers['user-agent'],
+        account_is_created: false,
+        created_at: new Date(),
+        updated_at: null,
+      }).then(() => {
+        res.json({ success: true });
+      });
+    } else {
+      res.json({ success: true });
+    }
+  }
 });
 
 router.get('/request_sms', (req, res) => {
