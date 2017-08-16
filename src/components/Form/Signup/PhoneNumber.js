cfe130844a48e7d465ca592dc4c5644134140bec
adcpm <fabien@bonustrack.co>
2017-08-16 20:14:30 +0700

    Add prefixSelector

--- src/components/Form/Signup/PhoneNumber.js
+++ src/components/Form/Signup/PhoneNumber.js
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Form, Input, Select, Button } from 'antd';
+import _ from 'lodash';
+import countries from '../../../../countries.json';
 
 class PhoneNumber extends React.Component {
   handleSubmit = (e) => {
@@ -15,24 +17,39 @@ class PhoneNumber extends React.Component {
   render() {
     const { getFieldDecorator } = this.props.form;
 
-    const prefixSelector = getFieldDecorator('prefix', {
-      initialValue: '86',
-    })(
-      <Select style={{ width: 60 }}>
-        <Select.Option value="86">+86</Select.Option>
-        <Select.Option value="87">+87</Select.Option>
+    const prefixSelector = getFieldDecorator('prefix')(
+      <Select
+        showSearch
+        optionFilterProp="children"
+        filterOption={(input, option) =>
+          option.props.label.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+      >
+        {countries.map(country => (
+          <Select.Option
+            key={_.uniqueId()}
+            value={country.prefix}
+            label={`${country.name} (+${country.prefix})`}
+          >
+            {`${country.name} (+${country.prefix})`}
+          </Select.Option>
+        ))}
       </Select>
     );
 
     return (
       <Form onSubmit={this.handleSubmit}>
         <Form.Item
+          label="Country Code"
+        >
+          {prefixSelector}
+        </Form.Item>
+        <Form.Item
           label="Phone Number"
         >
           {getFieldDecorator('phoneNumber', {
             rules: [{ required: true, message: 'Please input your phone number!' }],
           })(
-            <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
+            <Input />
           )}
         </Form.Item>
         <Form.Item>
