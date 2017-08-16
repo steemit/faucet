f85c99113d30e5ea5c698e30ca2fa94d8b0d3239
Grégory LATINIER <cehraphaim@live.fr>
2017-08-16 12:13:20 +0200

    Add routes squeleton for signup process

--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,20 @@ router.get('/', (req, res) => {
   res.json({});
 });
 
+router.get('/request_email', (req, res) => {
+  res.json({});
+});
+
+router.get('/request_sms', (req, res) => {
+  res.json({});
+});
+
+router.get('/confirm_sms', (req, res) => {
+  res.json({});
+});
+
+router.get('/confirm_email', (req, res) => {
+  res.json({});
+});
+
 module.exports = router;
