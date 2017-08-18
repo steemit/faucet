108b88140e26ad552eebc0781d1a47b3172fc0e9
Grégory LATINIER <cehraphaim@live.fr>
2017-08-18 09:18:18 +0200

    Add confirm-email route and logic

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Wrapper from './containers/app';
 import Signup from './components/Signup';
+import ConfirmEmail from './components/ConfirmEmail/Index';
 
 export default (
   <Route path="/" component={Wrapper}>
     <IndexRoute component={Signup} />
+    <Route path="confirm-email" component={ConfirmEmail} />
   </Route>
 );
