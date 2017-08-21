7620f80a209e08181bc0591cdabfe70a4ecf29b2
Grégory LATINIER <cehraphaim@live.fr>
2017-08-21 13:51:11 +0200

    Remove unused method

--- routes/api.js
+++ routes/api.js
@@ -112,20 +112,6 @@ router.get('/request_email', async (req, res) => {
   }
 });
 
-router.get('/submit_phone', (req, res) => {
-  const { countryCode, phoneNumber } = req.query;
-  req.twilio.messages.create({
-    body: '12345 is your SteemConnect confirmation code',
-    to: `+${countryCode}${phoneNumber}`,
-    from: '+12062028357',
-  }).then((result) => {
-    res.json(result);
-  }).catch((error) => {
-    const status = error.status || 400;
-    res.status(status).json(error);
-  });
-});
-
 router.get('/request_sms', async (req, res) => {
   let decoded;
   const errors = [];
