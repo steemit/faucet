0d656a52437d45cd5abf2eab3d9936dc5b2cd2e2
Grégory LATINIER <cehraphaim@live.fr>
2017-08-21 16:40:41 +0200

    Remove error final ponctuation

--- routes/api.js
+++ routes/api.js
@@ -18,7 +18,7 @@ const sendConfirmationEmail = async (req, res) => {
   const user = await req.db.users.findOne({ where: { email: req.query.email } });
 
   if (user.email_is_verified) {
-    const errors = [{ field: 'email', error: 'Email already verified.' }];
+    const errors = [{ field: 'email', error: 'Email already verified' }];
     res.status(400).json({ errors });
   } else if (
     !user.last_attempt_verify_email ||
@@ -45,12 +45,12 @@ const sendConfirmationEmail = async (req, res) => {
 
         res.json({ success: true, token });
       } else {
-        const errors = [{ field: 'form', error: 'Failed to send confirmation email.' }];
+        const errors = [{ field: 'form', error: 'Failed to send confirmation email' }];
         res.status(500).json({ errors });
       }
     });
   } else {
-    const errors = [{ field: 'form', error: 'Please wait at least one minute between retries.' }];
+    const errors = [{ field: 'form', error: 'Please wait at least one minute between retries' }];
     res.status(400).json({ errors });
   }
 };
@@ -58,7 +58,7 @@ const sendConfirmationEmail = async (req, res) => {
 router.get('/request_email', async (req, res) => {
   const errors = [];
   if (!req.query.email) {
-    errors.push({ field: 'email', error: 'Email is required.' });
+    errors.push({ field: 'email', error: 'Email is required' });
   } else if (!validator.isEmail(req.query.email)) {
     errors.push({ field: 'email', error: 'Please provide a valid email' });
   } else {
@@ -69,17 +69,17 @@ router.get('/request_email', async (req, res) => {
       },
     });
     if (userCount > 0) {
-      errors.push({ field: 'email', error: 'Email already used.' });
+      errors.push({ field: 'email', error: 'Email already used' });
     }
   }
 
   if (!req.query.recaptcha) {
-    errors.push({ field: 'recaptcha', error: 'Recaptcha is required.' });
+    errors.push({ field: 'recaptcha', error: 'Recaptcha is required' });
   } else {
     const response = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET}&response=${req.query.recaptcha}&remoteip=${req.ip}`);
     const body = await response.json();
     if (!body.success) {
-      errors.push({ field: 'recaptcha', error: 'Recaptcha is invalid.' });
+      errors.push({ field: 'recaptcha', error: 'Recaptcha is invalid' });
     }
   }
 
@@ -117,19 +117,19 @@ router.get('/request_sms', async (req, res) => {
   const errors = [];
 
   if (!req.query.token) {
-    errors.push({ field: 'form', error: 'Token is required.' });
+    errors.push({ field: 'form', error: 'Token is required' });
   }
   if (!req.query.phoneNumber) {
-    errors.push({ field: 'phoneNumber', error: 'Phone number is required.' });
+    errors.push({ field: 'phoneNumber', error: 'Phone number is required' });
   }
   if (!req.query.prefix) {
-    errors.push({ field: 'prefix', error: 'Country code is required.' });
+    errors.push({ field: 'prefix', error: 'Country code is required' });
   }
 
   try {
     decoded = jwt.verify(req.query.token, process.env.JWT_SECRET);
   } catch (err) {
-    errors.push({ field: 'form', error: 'Invalid token.' });
+    errors.push({ field: 'form', error: 'Invalid token' });
   }
   if (errors.length === 0) {
     if (decoded && decoded.type === 'signup') {
@@ -148,11 +148,11 @@ router.get('/request_sms', async (req, res) => {
         const oneMinLater = new Date(date.setTime(date.getTime() - 60000));
 
         if (!user) {
-          errors.push({ field: 'form', error: 'Unknown user.' });
+          errors.push({ field: 'form', error: 'Unknown user' });
         } else if (user.last_attempt_verify_phone_number &&
           user.last_attempt_verify_phone_number.getTime() > oneMinLater
         ) {
-          errors.push({ field: 'form', error: 'Please wait at least one minute between retries.' });
+          errors.push({ field: 'form', error: 'Please wait at least one minute between retries' });
         }
 
         const phoneExists = await req.db.users.count({
@@ -163,7 +163,7 @@ router.get('/request_sms', async (req, res) => {
         });
 
         if (phoneExists > 0) {
-          errors.push({ field: 'phoneNumber', error: 'Phone number already used.' });
+          errors.push({ field: 'phoneNumber', error: 'Phone number already used' });
         }
 
         if (errors.length === 0) {
@@ -190,7 +190,7 @@ router.get('/request_sms', async (req, res) => {
         errors.push({ field: 'prefix', error: `Invalid prefix: ${countryPrefix} ${countryCode}` });
       }
     } else {
-      errors.push({ field: 'form', error: 'Invalid token type.' });
+      errors.push({ field: 'form', error: 'Invalid token type' });
     }
   }
   if (errors.length > 0) {
@@ -203,16 +203,16 @@ router.get('/confirm_sms', async (req, res) => {
   const errors = [];
 
   if (!req.query.token) {
-    errors.push({ field: 'form', error: 'Token is required.' });
+    errors.push({ field: 'form', error: 'Token is required' });
   }
   if (!req.query.code) {
-    errors.push({ field: 'code', error: 'Code is required.' });
+    errors.push({ field: 'code', error: 'Code is required' });
   }
 
   try {
     decoded = jwt.verify(req.query.token, process.env.JWT_SECRET);
   } catch (err) {
-    errors.push({ field: 'form', error: 'Invalid token.' });
+    errors.push({ field: 'form', error: 'Invalid token' });
   }
 
   if (errors.length === 0) {
@@ -224,11 +224,11 @@ router.get('/confirm_sms', async (req, res) => {
       });
       if (user) {
         if (user.phone_number_is_verified) {
-          errors.push({ field: 'form', error: 'Phone already verified.' });
+          errors.push({ field: 'form', error: 'Phone already verified' });
         } else if (user.phone_code_attempts >= 5) {
-          errors.push({ field: 'form', error: 'Too many attempts, please request a new code.' });
+          errors.push({ field: 'form', error: 'Too many attempts, please request a new code' });
         } else if (user.phone_code !== req.query.code) {
-          errors.push({ field: 'code', error: 'Invalid code.' });
+          errors.push({ field: 'code', error: 'Invalid code' });
           req.db.users.update({
             phone_code_attempts: user.phone_code_attempts + 1,
           }, { where: { email: decoded.email } });
@@ -240,10 +240,10 @@ router.get('/confirm_sms', async (req, res) => {
           res.json({ success: true });
         }
       } else {
-        errors.push({ field: 'form', error: 'Unknown user.' });
+        errors.push({ field: 'form', error: 'Unknown user' });
       }
     } else {
-      errors.push({ field: 'form', error: 'Invalid token type.' });
+      errors.push({ field: 'form', error: 'Invalid token type' });
     }
   }
   if (errors.length > 0) {
@@ -253,7 +253,7 @@ router.get('/confirm_sms', async (req, res) => {
 
 router.get('/confirm_email', async (req, res) => {
   if (!req.query.token) {
-    res.status(400).json({ error: 'Token is required.' });
+    res.status(400).json({ error: 'Token is required' });
   } else {
     let decoded;
     try {
@@ -261,9 +261,9 @@ router.get('/confirm_email', async (req, res) => {
       if (decoded.type === 'confirm_email') {
         const user = await req.db.users.findOne({ where: { email: decoded.email } });
         if (!user) {
-          res.status(400).json({ error: 'Email doesn\'t exist.' });
+          res.status(400).json({ error: 'Email doesn\'t exist' });
         } else if (user.email_is_verified) {
-          res.status(400).json({ error: 'Email already verified.' });
+          res.status(400).json({ error: 'Email already verified' });
         } else {
           req.db.users.update({
             email_is_verified: true,
@@ -271,10 +271,10 @@ router.get('/confirm_email', async (req, res) => {
             .then(() => res.json({ success: true }));
         }
       } else {
-        res.status(400).json({ error: 'Invalid token.' });
+        res.status(400).json({ error: 'Invalid token' });
       }
     } catch (err) {
-      res.status(500).json({ error: 'Invalid token.' });
+      res.status(500).json({ error: 'Invalid token' });
     }
   }
 });
