aca324eaaf1bd39face858933510760faf40a1f4
Fabien <bonustrack@users.noreply.github.com>
2017-10-03 18:20:19 +0700

    Fix merge

--- src/components/Form/Signup/Email.js
+++ src/components/Form/Signup/Email.js
@@ -6,8 +6,6 @@ import fetch from 'isomorphic-fetch';
 import { checkStatus, parseJSON } from '../../../utils/fetch';
 import badDomains from '../../../../bad-domains';
 import fingerprint from '../../../../helpers/fingerprint';
-import RecaptchaItem from '../Recaptcha/RecaptchaItem';
-import { validateEmail } from '../../../utils/validator';
 
 class Email extends React.Component {
   constructor(props) {
@@ -58,8 +56,7 @@ class Email extends React.Component {
     this.setState({ submitting: true });
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
-        fetch(`/api/request_email?email=${encodeURIComponent(values.email)}&fingerprint=${this.state.fingerprint}&username=${this.props.username}&recaptcha=${window.grecaptcha.getResponse()}`)
-          .then(checkStatus)
+        fetch(`/api/request_email?email=${values.email}&fingerprint=${this.state.fingerprint}&username=${this.props.username}&recaptcha=${this.props.recaptcha || window.grecaptcha.getResponse()}`)  .then(checkStatus)
           .then(parseJSON)
           .then((data) => {
             this.setState({ submitting: false });
