231a7ca0450df59e020acb1e323c5975e18e16a0
Grégory <gregory.latinier@gmail.com>
2017-10-05 13:47:23 +0200

    Remove the recaptcha field from database

--- db/migrations/create-users.js
+++ db/migrations/create-users.js
@@ -61,9 +61,6 @@ module.exports = {
       username_booked_at: {
         type: Sequelize.DATE,
       },
-      recaptcha: {
-        type: Sequelize.STRING,
-      },
     })
   ),
   down: queryInterface => queryInterface.dropTable('users'),
