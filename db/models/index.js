71b1b76a470546abfdea473749b572e8bdb1672b
Grégory <gregory.latinier@gmail.com>
2017-12-22 17:27:35 +0100

    Change the database config to use the environment variable `DATABASE_URL`

--- db/models/index.js
+++ db/models/index.js
@@ -4,8 +4,7 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env       = process.env.DATABASE_NAME || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
+var config    = require(__dirname + '/../config/config.json');
 var db        = {};
 
 if (config.use_env_variable) {
