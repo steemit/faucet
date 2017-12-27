b04906dcad09f5aef21bb423e675b63319bdd596
Grégory LATINIER <gregory.latinier@gmail.com>
2017-12-27 17:24:56 +0100

    Add the healthcheck endpoint (#112)

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
+router.get('/.well-known/healthcheck.json', (req, res) => {
+  res.json({ ok: true, date: new Date().toISOString() });
+});
+
 router.get('/*', (req, res) => {
   res.render('index', { title: 'SteemConnect' });
 });
