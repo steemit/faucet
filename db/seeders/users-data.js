9ecbde8d648ba6d874dd3ae9b4cf866843b865fe
Iain Maitland <imaitland@gmail.com>
2018-04-02 06:09:10 -0500

    never verified email user

--- db/seeders/users-data.js
+++ db/seeders/users-data.js
@@ -1,3 +1,6 @@
+const moment = require('moment');
+const twoWeeksAgo = moment().subtract(14, 'days').format("YYYY-MM-DD HH:mm:ss");
+
 module.exports = {
   up: queryInterface => (
     queryInterface.bulkInsert('users', [
@@ -62,7 +65,27 @@ module.exports = {
         metadata: JSON.stringify({ query: { uid: '12345' } }),
         account_is_created: false,
         email_is_verified: false,
-      }
+      },
+      // Stopped at step 1 - User was sent validation email but has not verified within 14 days.
+      {
+        created_at:	'2018-03-12T21:17:14',
+        updated_at: '2018-03-15T18:57:46',
+        status: null,
+        last_attempt_verify_email: twoWeeksAgo,
+        username:	'neveremail',
+        username_booked_at:	'2018-03-12T21:17:14',
+        email:	'zzzz@gggm.com',
+        phone_number:	null,
+        last_attempt_verify_phone_number: null,
+        phone_code_attempts: 0,
+        phone_code:	null,
+        ip: '75.662.77.122',
+        fingerprint: '{"date": "Fri Sep 15 2017 10:38:36 GMT+0200 (Paris, Madrid (heure d’été)", "device": {"renderer": "ANGLE (Intel(R) HD Graphics 4000 Direct3D11 vs_5_0 ps_5_0)", "vendor": "Google Inc."}, "lang": "fr-FR,fr,en-US,en,ms", "ref": "https://jsfiddle.net/", "ua": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"}',
+        creation_hash: '5a234b0a964be4a73c0bf78df675038e1e297c4726cd7340bfeeaf036ceeb885',
+        metadata: JSON.stringify({ query: { uid: '23490' } }),
+        account_is_created: false,
+        email_is_verified: false,
+      },
     ], {})
   ),
   down: queryInterface => queryInterface.bulkDelete('users', null, {}),
