89a5bb108a6231856b81320027303c29681c3329
Iain Maitland <imaitland@gmail.com>
2018-04-26 14:46:00 -0500

    add a test for the to-js hoc, refs #321

---
+++ src/utils/to-js.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Map } from 'immutable';
+import toJS from './to-js';
+
+const MockComponent = props => <h1> Hello {props.name} </h1>;
+
+const mockProps = {
+    name: 'Foo',
+};
+
+const mockImmutableProps = Map({
+    name: 'Foo',
+});
+
+describe('to-js', () => {
+    test('to-js passes props object', () => {
+        const received = toJS(MockComponent)(mockProps);
+        const expected = <MockComponent name="Foo" />;
+        expect(received).toEqual(expected);
+    });
+    test('to-js converts Immutable Map to object', () => {
+        const received = toJS(MockComponent)(mockImmutableProps);
+        const expected = (
+            <MockComponent
+                __altered={false}
+                __hash={undefined}
+                __ownerID={undefined}
+                _root={{ entries: [['name', 'Foo']], ownerID: {} }}
+                size={1}
+            />
+        );
+        expect(received).toEqual(expected);
+    });
+});
