0d74f133fc02dffd4b654654cc7b8a0f0c7c2ef6
Benjamin Chodoroff <bc@thermitic.net>
2018-05-30 15:13:49 -0400

    add comment containing manual migration step

--- db/migrations/004.normalize-emails.js
+++ db/migrations/004.normalize-emails.js
@@ -1,5 +1,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // Manual migration step:
+    // UPDATE users SET email_normalized = LOWER(CONCAT(REPLACE(SUBSTRING_INDEX(SUBSTRING_INDEX(email, '@', 1), '+', 1), '.', ''), '@', SUBSTRING_INDEX(email, '@', -1)));
     await queryInterface.addColumn('users', 'email_normalized', {
       type: Sequelize.STRING,
       allowNull: true,
