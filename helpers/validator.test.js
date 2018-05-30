71047f7695d9c90a29cdb3301671882efa51f7d1
Iain Maitland <imaitland@gmail.com>
2018-05-30 08:26:48 -0500

    more comprehensive tests for email normalizer

--- helpers/validator.test.js
+++ helpers/validator.test.js
@@ -14,3 +14,69 @@ describe('normalizeEmail', () => {
         });
     });
 });
+
+const gmailEmailsToNormalize = [
+    'johnotander@gmail.com',
+    'johnotander@googlemail.com',
+    'johnotander@GMAIL.com',
+    'johnotander+foobar@gmail.com',
+    'john.o.t.a.n.d.er+foobar@gmail.com',
+    'JOHN.o.t.a.n.d.er+foobar@googlemail.com',
+    'john.otander@gmail.com',
+];
+
+const hotmailEmailsToNormalize = [
+    'johnotander@hotmail.com',
+    'johnotander@hotmail.com',
+    'johnotander@HOTMAIL.com',
+    'Johnotander@hotmail.com',
+];
+
+const liveEmailsToNormalize = [
+    'johnotander@live.com',
+    'johnotander@live.com',
+    'johnotander@live.com',
+    'johnotander+foobar@live.com',
+    'john.o.t.a.n.d.er+foobar@live.com',
+    'JOHN.o.t.a.n.d.er+foobar@live.com',
+    'john.otander@live.com',
+];
+
+const outlookEmailsToNormalize = [
+    'john.otander@outlook.com',
+    'JOHN.otander@outlook.com',
+    'john.Otander+any.label@outlook.com',
+    'john.otander+foobar@outlook.com',
+];
+
+describe('normalize-email', () => {
+    it('should normalize gmail emails', () => {
+        gmailEmailsToNormalize.forEach(email => {
+            expect(normalizeEmail(email)).toEqual('johnotander@gmail.com');
+        });
+    });
+
+    it('should normalize hotmail emails', () => {
+        hotmailEmailsToNormalize.forEach(email => {
+            expect(normalizeEmail(email)).toEqual('johnotander@hotmail.com');
+        });
+    });
+
+    it('should not remove dots from hotmail emails', () => {
+        expect(normalizeEmail('john.otander@hotmail.com')).toEqqual(
+            'john.otander@hotmail.com'
+        );
+    });
+
+    it('should normalize live emails', () => {
+        liveEmailsToNormalize.forEach(email => {
+            expect(normalizeEmail(email)).toEqual('johnotander@live.com');
+        });
+    });
+
+    it('should normalize outlook emails', () => {
+        outlookEmailsToNormalize.forEach(email => {
+            expect(normalizeEmail(email)).toEqual('john.otander@outlook.com');
+        });
+    });
+});
