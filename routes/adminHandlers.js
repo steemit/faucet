7c4fc61d98d82718e8404a10eaa1d3a46941bb52
Iain Maitland <imaitland@gmail.com>
2018-06-13 16:50:39 -0500

    testing of adminHandlers

---
+++ routes/adminHandlers.js
@@ -0,0 +1,134 @@
+const database = require('../helpers/database');
+
+const { Sequelize } = require('../db/models');
+
+async function listSignups(req) {
+    const { limit, order, offset, filters } = req.body;
+    const query = {
+        order: order || [['created_at', 'DESC']],
+        limit: limit ? Math.min(limit, 100) : 10,
+    };
+    if (offset) {
+        query.offset = offset;
+    }
+    if (Array.isArray(filters) && filters.length > 0) {
+        const {
+            where,
+            literal,
+            Op: { or, eq, ne, like, notLike, and, gte, lte, regexp, notRegexp },
+        } = Sequelize;
+
+        const andList = [];
+        for (const filter of filters) {
+            // eslint-disable-line
+            const { value } = filter;
+            let name = filter.name;
+            let negate = false;
+            if (name[0] === '!') {
+                negate = true;
+                name = name.slice(1);
+            }
+            const nLike = negate ? notLike : like;
+            const nEq = negate ? ne : eq;
+            const nRegexp = negate ? notRegexp : regexp;
+            switch (name) {
+                case 'text':
+                    andList.push({
+                        [negate ? and : or]: [
+                            { email: { [nLike]: `%${value}%` } },
+                            { username: { [nLike]: `%${value}%` } },
+                            { phone_number: { [nLike]: `%${value}%` } },
+                        ],
+                    });
+                    break;
+                case 'status':
+                    andList.push({ status: { [nEq]: value } });
+                    break;
+                case 'ip':
+                    andList.push({ ip: { [nEq]: value } });
+                    break;
+                case 'username':
+                    andList.push({ username: { [nEq]: value } });
+                    break;
+                case 'phone':
+                case 'phone_number':
+                    andList.push({ phone_number: { [nEq]: value } });
+                    break;
+                case 'email':
+                    andList.push({ email: { [nEq]: value } });
+                    break;
+                case 'username_re':
+                    andList.push({ username: { [nRegexp]: value } });
+                    break;
+                case 'email_re':
+                    andList.push({ email: { [nRegexp]: value } });
+                    break;
+                case 'phone_re':
+                case 'phone_number_re':
+                    andList.push({ phone_number: { [nRegexp]: value } });
+                    break;
+                case 'note':
+                    andList.push({ review_note: { [nLike]: `%${value}%` } });
+                    break;
+                case 'note_re':
+                    andList.push({ review_note: { [nRegexp]: value } });
+                    break;
+                case 'from':
+                    andList.push({ created_at: { [gte]: new Date(value) } });
+                    break;
+                case 'to':
+                    andList.push({ created_at: { [lte]: new Date(value) } });
+                    break;
+                case 'fingerprint.ua':
+                    andList.push({
+                        fingerprint: where(literal("fingerprint -> '$.ua'"), {
+                            [nRegexp]: value,
+                        }),
+                    });
+                    break;
+                case 'fingerprint.ref':
+                    andList.push({
+                        fingerprint: where(literal("fingerprint -> '$.ref'"), {
+                            [nRegexp]: value,
+                        }),
+                    });
+                    break;
+                case 'fingerprint.lang':
+                    andList.push({
+                        fingerprint: where(literal("fingerprint -> '$.lang'"), {
+                            [nRegexp]: value,
+                        }),
+                    });
+                    break;
+                case 'fingerprint.device.vendor':
+                    andList.push({
+                        fingerprint: where(
+                            literal("fingerprint -> '$.device.vendor'"),
+                            { [nRegexp]: value }
+                        ),
+                    });
+                    break;
+                case 'fingerprint.device.renderer':
+                    andList.push({
+                        fingerprint: where(
+                            literal("fingerprint -> '$.device.renderer'"),
+                            { [nRegexp]: value }
+                        ),
+                    });
+                    break;
+                default:
+                    throw new Error(`Unknown filter: ${name}`);
+            }
+        }
+        query.where = { [and]: andList };
+    }
+    const [total, users] = await Promise.all([
+        database.countUsers(query.where),
+        database.findUsers(query),
+    ]);
+    return { total, users, query };
+}
+
+module.exports = {
+    listSignups,
+};
---
+++ routes/adminHandlers.test.js
@@ -0,0 +1,147 @@
+const { Sequelize } = require('./../db/models');
+
+const { Op: { or, eq, ne, like, and, gte, lte, regexp } } = Sequelize;
+
+describe('adminHandlers listSignups', () => {
+    beforeEach(() => {
+        jest.resetModules();
+    });
+    it('should find and count users', async () => {
+        jest.mock('../helpers/database');
+        const adminHandlers = require('./adminHandlers');
+        const mockDb = require('../helpers/database');
+        adminHandlers.listSignups({
+            body: {
+                limit: 60,
+                order: 'fun',
+                offset: 30,
+                filters: [{ name: 'status', value: 'test' }],
+            },
+        });
+        expect(mockDb.findUsers.mock.calls.length).toBe(1);
+        expect(mockDb.countUsers.mock.calls.length).toBe(1);
+    });
+    it('negates correctly when search text is prepended with !', async () => {
+        const adminHandlers = require('./adminHandlers');
+        const ret = await adminHandlers.listSignups({
+            body: {
+                limit: 60,
+                order: 'fun',
+                offset: 30,
+                filters: [
+                    { name: 'status', value: 'status_test' },
+                    { name: '!status', value: 'ne_status_test' },
+                ],
+            },
+        });
+        const q = ret.query.where[and];
+        expect(q[0].status[eq]).toBe('status_test');
+        expect(q[1].status[ne]).toBe('ne_status_test');
+    });
+    it('throws an error when unknown filter is passed', async () => {
+        const adminHandlers = require('./adminHandlers');
+        try {
+            await adminHandlers.listSignups({
+                body: {
+                    limit: 60,
+                    order: 'fun',
+                    offset: 30,
+                    filters: [{ name: 'idk', value: 'nope' }],
+                },
+            });
+        } catch (e) {
+            expect(e).toEqual(new Error('Unknown filter: idk'));
+        }
+    });
+
+    it('creates the correct query for an array of different filters', async () => {
+        const adminHandlers = require('./adminHandlers');
+        const ret = await adminHandlers.listSignups({
+            body: {
+                limit: 60,
+                order: 'fun',
+                offset: 30,
+                filters: [
+                    { name: 'text', value: 'text_test' },
+                    { name: 'status', value: 'status_test' },
+                    { name: '!status', value: 'ne_status_test' },
+                    { name: 'ip', value: 'ip_test' },
+                    { name: 'phone', value: 'phone_test' },
+                    { name: 'email', value: 'email_test' },
+                    { name: 'username_re', value: 'username_re_test' },
+                    { name: 'email_re', value: 'email_re_test' },
+                    { name: 'phone_re', value: 'phone_re_test' },
+                    { name: 'note', value: 'note_test' },
+                    { name: 'note_re', value: 'note_re_test' },
+                    { name: 'from', value: 'December 17, 1995 03:24:00' },
+                    { name: 'to', value: 'December 17, 1995 04:24:00' },
+                    {
+                        name: 'fingerprint.ua',
+                        value: 'fingerprint_ua_test',
+                    },
+                    {
+                        name: 'fingerprint.ref',
+                        value: 'fingerprint_ref_test',
+                    },
+                    {
+                        name: 'fingerprint.lang',
+                        value: 'fingerprint_lang_test',
+                    },
+                    {
+                        name: 'fingerprint.device.vendor',
+                        value: 'fingerprint_vendor_test',
+                    },
+                    {
+                        name: 'fingerprint.device.renderer',
+                        value: 'fingerprint_renderer_test',
+                    },
+                ],
+            },
+        });
+        const q = ret.query.where[and];
+        expect(q[0][or][0].email[like]).toBe('%text_test%');
+        expect(q[0][or][1].username[like]).toBe('%text_test%');
+        expect(q[0][or][2].phone_number[like]).toBe('%text_test%');
+        expect(q[1].status[eq]).toBe('status_test');
+        expect(q[2].status[ne]).toBe('ne_status_test');
+        expect(q[3].ip[eq]).toBe('ip_test');
+        expect(q[4].phone_number[eq]).toBe('phone_test');
+        expect(q[5].email[eq]).toBe('email_test');
+        expect(q[6].username[regexp]).toBe('username_re_test');
+        expect(q[7].email[regexp]).toBe('email_re_test');
+        expect(q[8].phone_number[regexp]).toBe('phone_re_test');
+        expect(q[9].review_note[like]).toBe('%note_test%');
+        expect(q[10].review_note[regexp]).toBe('note_re_test');
+        expect(q[11].created_at[gte]).toEqual(
+            new Date('1995-12-17T09:24:00.000Z')
+        );
+        expect(q[12].created_at[lte]).toEqual(
+            new Date('1995-12-17T10:24:00.000Z')
+        );
+        expect(q[13].fingerprint).toEqual({
+            attribute: { val: "fingerprint -> '$.ua'" },
+            comparator: '=',
+            logic: { [regexp]: 'fingerprint_ua_test' },
+        });
+        expect(q[14].fingerprint).toEqual({
+            attribute: { val: "fingerprint -> '$.ref'" },
+            comparator: '=',
+            logic: { [regexp]: 'fingerprint_ref_test' },
+        });
+        expect(q[15].fingerprint).toEqual({
+            attribute: { val: "fingerprint -> '$.lang'" },
+            comparator: '=',
+            logic: { [regexp]: 'fingerprint_lang_test' },
+        });
+        expect(q[16].fingerprint).toEqual({
+            attribute: { val: "fingerprint -> '$.device.vendor'" },
+            comparator: '=',
+            logic: { [regexp]: 'fingerprint_vendor_test' },
+        });
+        expect(q[17].fingerprint).toEqual({
+            attribute: { val: "fingerprint -> '$.device.renderer'" },
+            comparator: '=',
+            logic: { [regexp]: 'fingerprint_renderer_test' },
+        });
+    });
+});
