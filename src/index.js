145fabfdaa0e1421f992e412aa03d41a093c7b0b
ety001 <work@akawa.ink>
2024-08-12 01:50:38 +0800

    set prettier configuration semi true

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 
 // Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>'
+document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
-const root = createRoot(document.getElementById('app'))
-root.render(<h1>Hello, world</h1>)
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
