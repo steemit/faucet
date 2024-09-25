c3a88e9e28f271759c40f2498d463ca6eaf260e9
ety001 <work@akawa.ink>
2024-09-25 12:47:33 +0800

    fix style

--- db/models/phonecode.js
+++ db/models/phonecode.js
@@ -1,15 +1,15 @@
 export default (sequelize, DataTypes) => (
-    sequelize.define('phonecode', {
-        phone_number: DataTypes.STRING,
-        phone_code_attempts: DataTypes.INTEGER,
-        phone_code: DataTypes.STRING,
-        last_attempt_verify_phone_number: DataTypes.DATE,
-        phone_code_sent: DataTypes.INTEGER,
-        phone_code_generated: DataTypes.DATE,
-        phone_code_first_sent: DataTypes.DATE,
-    }, {
-        freezeTableName: true,
-        underscored: true,
-        timestamps: false,
-    })
+  sequelize.define('phonecode', {
+    phone_number: DataTypes.STRING,
+    phone_code_attempts: DataTypes.INTEGER,
+    phone_code: DataTypes.STRING,
+    last_attempt_verify_phone_number: DataTypes.DATE,
+    phone_code_sent: DataTypes.INTEGER,
+    phone_code_generated: DataTypes.DATE,
+    phone_code_first_sent: DataTypes.DATE,
+  }, {
+    freezeTableName: true,
+    underscored: true,
+    timestamps: false,
+  })
 );
